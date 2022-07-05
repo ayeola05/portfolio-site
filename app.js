3384913f3874f5534413683171196ed3af4ec72b
Taiwo <ayeola05@gmail.com>
2022-07-05 09:57:04 +0100

    Edited Portfolio

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ function PageTransitions() {
       element.classList.add("active");
     }
   });
+
+  //Toggle Theme
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
 }
 
 PageTransitions();
